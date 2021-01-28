0f91878c190ac7eff414533fa4dc8482a195b879
Thành Đạt Lê <jamesgrantellison@live.com>
2021-01-28 03:53:29 +0000

    Update Resource.java

--- src/main/java/com/dtl/demo/service/Resource.java
+++ src/main/java/com/dtl/demo/service/Resource.java
@@ -14,9 +14,9 @@ public class Resource {
     public Response message() {
 	StringBuilder builder = new StringBuilder();
 	builder.append("{");
-	builder.append(String.format("\"%s\": \"%s\",", "os.arch", System.getProperty("os.arch")));
-	builder.append(String.format("\"%s\": \"%s\",", "os.name", System.getProperty("os.name")));
-	builder.append(String.format("\"%s\": \"%s\"", "os.version", System.getProperty("os.version")));
+	builder.append(String.format("\"%s\": \"%s\",", "OS Arch", System.getProperty("os.arch")));
+	builder.append(String.format("\"%s\": \"%s\",", "OS Name", System.getProperty("os.name")));
+	builder.append(String.format("\"%s\": \"%s\"", "OS Version", System.getProperty("os.version")));
 	builder.append("}");
         return Response.ok(builder.toString()).build();
     }
