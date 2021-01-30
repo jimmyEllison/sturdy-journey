1dc5a38512adb4bbac669ef10b817568b0cc4cc9
ThanhDatLe <JamesGrantEllison@live.com>
2021-01-30 11:19:54 +0700

    Update

--- src/main/java/com/dtl/demo/service/Resource.java
+++ src/main/java/com/dtl/demo/service/Resource.java
@@ -4,20 +4,19 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 @Path("properties")
 public class Resource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response message() {
-	StringBuilder builder = new StringBuilder();
-	builder.append("{");
-	builder.append(String.format("\"%s\": \"%s\",", "OS Arch", System.getProperty("os.arch")));
-	builder.append(String.format("\"%s\": \"%s\",", "OS Name", System.getProperty("os.name")));
-	builder.append(String.format("\"%s\": \"%s\"", "OS Version", System.getProperty("os.version")));
-	builder.append("}");
-        return Response.ok(builder.toString()).build();
+    public String getSystemInfo() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("{");
+        builder.append(String.format("\"%s\": \"%s\",", "arch", System.getProperty("os.arch")));
+        builder.append(String.format("\"%s\": \"%s\",", "name", System.getProperty("os.name")));
+        builder.append(String.format("\"%s\": \"%s\"", "version", System.getProperty("os.version")));
+        builder.append("}");
+        return builder.toString();
     }
 }
